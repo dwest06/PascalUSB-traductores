1f52cfd08b8d81411856c4632a7d3c8ad39cf139
David Rod <dave00dark@gmail.com>
2018-11-09 12:30:17 -0400

    Añadido inicio de Parser.py

---
+++ parser.py
@@ -0,0 +1,15 @@
+"""
+    Analizador Sintactico del Lenguaje PascalUSB
+
+    Segunda fase del proyecto
+
+    Traductores e Interpretadores (CI-3725)
+    David Rodríguez (14-10930)
+    Javier Medina (12-10400)
+
+    Septiembre - Diciembre 2018
+"""
+
+from sys import argv
+from lexer import TOKENS_VALIDOS
+
