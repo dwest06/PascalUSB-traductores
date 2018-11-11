8a7f2f6b0d2ced8769dd0ae0ac4adf75b8703922
javtrv <jav393@gmail.com>
2018-11-11 00:59:17 -0400

    Se agregaron mas producciones a parser.py

--- parser.py
+++ parser.py
@@ -34,7 +34,8 @@ def p_instruccion(p):
 				| SALIDA
 				| CONDICIONAL_IF
 				| CONDICIONAL_CASE
-				| ITERACION
+				| ITERACION_FOR
+				| ITERACION_WHILE
 				| INSTRUCCION INSTRUCCION
 	"""
 	#if  len(p) == 2:
@@ -113,4 +114,63 @@ def p_lista_case(p):
 	LISTA_CASE : EXPRESION TkArrow INSTRUCCION
 			   | EXPRESION TkArrow INSTRUCCION LISTA_CASE
 	"""
-	
+	#if len(p) == 4:
+		#p[0] = lista_case([p[1],p[3]])
+	#else
+		#p[0] = lista_case([p[1],p[3],p[4]])
+	print "lista case"
+
+def p_iteracion_for(p):
+	"""
+	ITERACION_FOR : TkFor IDENTIFICADOR TkIn EXPRESION TkDo INSTRUCCION
+	"""
+	#p[0] = iteracion_for([p[2],p[4],p[6]])
+	print "for"
+
+def p_iteracion_while(p):
+	"""
+	ITERACION_WHILE : TkWhile EXPBOOL TkDo INSTRUCCION
+	"""
+	#p[0] = iteracion_while([p[2],p[4]])
+	print "while"
+
+def p_declaracion_var(p):
+	"""
+	DECL_VAR : LISTA_VAR TkAs LISTA_TIPO TkSemicolon
+	         | LISTA_VAR TkAs LISTA_TIPO
+	"""
+	#p[0] = lista_var([p[1],p[3]])
+	print "declaracion"
+
+def p_lista_var(p):
+	"""
+	LISTA_VAR : IDENTIFICADOR
+			  | IDENTIFICADOR TkComma LISTA_VAR
+	"""
+	#if len(p) == 2:
+		#p[0] = lista_var([p[1]])
+	#else
+		#p[0] = lista_var([p[1],p[3]])
+
+	print "lista var"
+
+def p_lista_tipo(p):
+	"""
+	LISTA_TIPO : DATO
+			  | DATO TkComma LISTA_TIPO
+	"""
+	#if len(p) == 2:
+		#p[0] = lista_dato([p[1]])
+	#else
+		#p[0] = lista_dato([p[1],p[3]])
+
+	print "lista dato"
+
+def p_dato(p):
+	"""
+	DATO : TkInt
+		 | TkBool
+		 | TKInter
+	"""
+	#p[0] = dato([p[1]])
+ 
