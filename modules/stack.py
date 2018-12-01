351b481038d00161fe5c4c4352ef559062f1e37a
David Rod <dave00dark@gmail.com>
2018-11-30 23:36:32 -0400

    Modulos de Stack y Tabla de Simbolos

---
+++ modules/stack.py
@@ -0,0 +1,29 @@
+class Stack(object):
+    """docstring for Stack"""
+    def __init__(self):
+        super(Stack, self).__init__()
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.isEmpty:
+            return self.items.pop()
+
+    def top(self):
+        if not self.isEmpty:
+            return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        lista = ''
+        for i in range(len(self.items)):
+            lista += '[' + str(self.items[i]) + '],' + '\n'
+        return lista
+        
